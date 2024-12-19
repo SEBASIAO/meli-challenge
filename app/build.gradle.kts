5ec6e37a706b6f5e46846264409fc7edd8da58b9
Sebastian Garcia <sebastian.garcia@y.uno>
2024-12-19 11:58:52 -0500

    chore(di): add Android Hilt configuration
    Configured Hilt for dependency injection in the project. Changes include:
    
    - Added Hilt Gradle dependencies to the build script.
    - Set up the @HiltAndroidApp annotation in the Application class.
    
    This provides a foundation for using Hilt as the DI engine across the app.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.org.jetbrains.kotlin.kapt)
+    alias(libs.plugins.hilt.android.plugin)
 }
 
 android {
@@ -40,7 +42,17 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
+    //Hilt
+    implementation(libs.hilt.android)
+    kapt(libs.hilt.compiler)
+}
+
+kapt{
+    correctErrorTypes = true
 }
