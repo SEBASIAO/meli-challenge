c3d114cd1c2f576ada963c996bdb3fd915a37f57
Sebastian Garcia <sebastian.garcia@y.uno>
2024-12-25 13:08:49 -0500

    feat(Network): Catch any exception when network call is executed

--- app/src/main/java/com/example/melichallenge/data/remote/datasource/BaseRemoteDataSource.kt
+++ app/src/main/java/com/example/melichallenge/data/remote/datasource/BaseRemoteDataSource.kt
@@ -1,16 +1,22 @@
 package com.example.melichallenge.data.remote.datasource
 
+import android.accounts.NetworkErrorException
 import retrofit2.Response
 
 abstract class BaseRemoteDataSource {
 
     protected suspend fun <T> getResult(call: suspend () -> Response<T>): Resource<T> {
-        val response = call()
-        return if (response.isSuccessful) {
-            Resource.success(response.body())
-        } else {
-            Resource.error(response.message())
+        try {
+            val response = call()
+            return if (response.isSuccessful) {
+                Resource.success(response.body())
+            } else {
+                Resource.error(response.message())
+            }
+        } catch (e : Exception){
+            return Resource.error("Network Call Error: $e")
         }
+
     }
 }
 
