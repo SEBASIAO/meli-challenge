24e2f39d91bb4459e973e3fab2c90434006ca18a
Sebastian Garcia <sebastian.garcia@y.uno>
2024-12-26 13:29:28 -0500

    refactor(viewModel, activity): migrate from LiveData to StateFlow for better state management
    - Replaced LiveData with StateFlow in ViewModel to leverage its ability to persist state across configuration changes.
    - Updated Activity to observe StateFlow using repeatOnLifecycle for lifecycle-aware data collection.
    - Simplified UI state handling and ensured RecyclerView data and loading states persist seamlessly.
    - Removed dependency on adapter-managed loading states, shifting full responsibility to ViewModel.
    
    This refactor improves scalability, resilience to configuration changes, and enhances the separation of concerns by letting ViewModel fully manage state.

--- app/src/main/java/com/example/melichallenge/presentation/home/MainActivity.kt
+++ app/src/main/java/com/example/melichallenge/presentation/home/MainActivity.kt
@@ -1,36 +1,48 @@
 package com.example.melichallenge.presentation.home
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
+import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.melichallenge.databinding.ActivityMainBinding
 import com.example.melichallenge.presentation.adapters.ItemAdapter
 import com.example.melichallenge.presentation.adapters.PaginationScrollListener
 import com.example.melichallenge.presentation.details.DetailsActivity
-import com.google.android.material.internal.ViewUtils.hideKeyboard
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private val viewModel: MainViewModel by viewModels()
-    private var isLoadingMoreItems = false
+    private val adapter = ItemAdapter { itemClicked ->
+        viewModel.getItemDetails(itemClicked)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.lifecycleOwner = this
-        setUpItemsRecyclerView()
 
-        binding.searchTf.setOnEditorActionListener { textView, actionId, event ->
+        setUpItemsRecyclerView()
+        setUpSearchInput()
+        observeViewModel()
+    }
+
+    private fun setUpSearchInput() {
+        binding.searchTf.setOnEditorActionListener { textView, actionId, _ ->
             if (actionId == EditorInfo.IME_ACTION_DONE) {
                 val query = binding.searchTf.text.toString()
                 viewModel.searchItemsByQuery(query)
@@ -43,9 +55,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setUpItemsRecyclerView() {
-        val adapter = ItemAdapter { itemClicked ->
-            viewModel.getItemDetails(itemClicked)
-        }
         val layoutManager = LinearLayoutManager(this)
         binding.searchRv.adapter = adapter
         binding.searchRv.layoutManager = layoutManager
@@ -57,55 +66,67 @@ class MainActivity : AppCompatActivity() {
 
             override fun isLastPage(): Boolean = viewModel.isLastPage
 
-            override fun isLoading(): Boolean = isLoadingMoreItems
-
+            override fun isLoading(): Boolean = viewModel.isLoadingMoreItems.value
         })
 
-        viewModel.uiState.observe(this) { state ->
-            when (state) {
-                is MainActivityUiState.Error -> {
-                    binding.progressCircular.isVisible = false
-                    binding.shimmerListContainer.root.isVisible = false
-                    isLoadingMoreItems = false
-                    adapter.hideLoading()
-                    Toast.makeText(this, state.message, Toast.LENGTH_LONG).show()
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.currentItems.collect { items ->
+                    adapter.setNewItems(items)
                 }
+            }
+        }
+    }
 
-                is MainActivityUiState.SuccessMoreItems -> {
-                    adapter.appendList(state.data)
-                    binding.progressCircular.isVisible = false
-                    binding.shimmerListContainer.root.isVisible = false
-                    isLoadingMoreItems = false
-                    adapter.hideLoading()
-                }
+    private fun observeViewModel() {
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.uiState.collect { state ->
+                    when (state) {
+                        is MainActivityUiState.Error -> {
+                            binding.progressCircular.isVisible = false
+                            binding.shimmerListContainer.root.isVisible = false
+                            adapter.hideLoading()
+                            Toast.makeText(this@MainActivity, state.message, Toast.LENGTH_LONG)
+                                .show()
+                        }
 
-                is MainActivityUiState.SuccessNewItems -> {
-                    adapter.setNewItems(state.data)
-                    binding.progressCircular.isVisible = false
-                    binding.shimmerListContainer.root.isVisible = false
-                    isLoadingMoreItems = false
-                    adapter.hideLoading()
-                }
+                        is MainActivityUiState.SuccessMoreItems -> {
+                            binding.progressCircular.isVisible = false
+                            binding.shimmerListContainer.root.isVisible = false
+                            adapter.hideLoading()
+                        }
 
-                MainActivityUiState.LoadingMoreItems -> {
-                    binding.progressCircular.isVisible = false
-                    binding.shimmerListContainer.root.isVisible = false
-                    isLoadingMoreItems = true
-                    adapter.showLoading()
-                }
+                        is MainActivityUiState.SuccessNewItems -> {
+                            binding.progressCircular.isVisible = false
+                            binding.shimmerListContainer.root.isVisible = false
+                            adapter.hideLoading()
+                        }
 
-                MainActivityUiState.LoadingNewItems -> {
-                    binding.progressCircular.isVisible = false
-                    binding.shimmerListContainer.root.isVisible = true
-                    isLoadingMoreItems = false
-                    adapter.hideLoading()
-                }
+                        MainActivityUiState.LoadingMoreItems -> {
+                            binding.progressCircular.isVisible = false
+                            binding.shimmerListContainer.root.isVisible = false
+                            adapter.showLoading()
+                        }
 
-                MainActivityUiState.Loading -> {
-                    binding.progressCircular.isVisible = true
-                    binding.shimmerListContainer.root.isVisible = false
-                    isLoadingMoreItems = false
-                    adapter.hideLoading()
+                        MainActivityUiState.LoadingNewItems -> {
+                            binding.progressCircular.isVisible = false
+                            binding.shimmerListContainer.root.isVisible = true
+                            adapter.hideLoading()
+                        }
+
+                        MainActivityUiState.Loading -> {
+                            binding.progressCircular.isVisible = true
+                            binding.shimmerListContainer.root.isVisible = false
+                            adapter.hideLoading()
+                        }
+
+                        MainActivityUiState.Idle -> {
+                            binding.progressCircular.isVisible = false
+                            binding.shimmerListContainer.root.isVisible = false
+                            adapter.hideLoading()
+                        }
+                    }
                 }
             }
         }
@@ -121,9 +142,16 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun hideKeyboard(view: View) {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
 }
 
 sealed class MainActivityUiState<out T> {
+    object Idle : MainActivityUiState<Nothing>()
+
     object Loading : MainActivityUiState<Nothing>()
 
     object LoadingNewItems : MainActivityUiState<Nothing>()
--- app/src/main/java/com/example/melichallenge/presentation/home/MainViewModel.kt
+++ app/src/main/java/com/example/melichallenge/presentation/home/MainViewModel.kt
@@ -8,16 +8,25 @@ import com.example.melichallenge.data.remote.datasource.Resource
 import com.example.melichallenge.domain.models.Item
 import com.example.melichallenge.domain.repository.SearchRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(private val repository: SearchRepository) : ViewModel() {
-    private val _uiState = MutableLiveData<MainActivityUiState<List<Item>>>()
-    val uiState: LiveData<MainActivityUiState<List<Item>>> get() = _uiState
+
+    private val _uiState = MutableStateFlow<MainActivityUiState<List<Item>>>(MainActivityUiState.Idle)
+    val uiState: StateFlow<MainActivityUiState<List<Item>>> get() = _uiState
+
+    private val _currentItems = MutableStateFlow<List<Item>>(emptyList())
+    val currentItems: StateFlow<List<Item>> get() = _currentItems
 
     private val _navigationEvent = MutableLiveData<MainActivityEvent<Item>>()
     val navigationEvent: LiveData<MainActivityEvent<Item>> get() = _navigationEvent
+
+    private val _isLoadingMoreItems = MutableStateFlow(false)
+    val isLoadingMoreItems: StateFlow<Boolean> get() = _isLoadingMoreItems
 
     private var currentQuery = ""
     private var currentOffset = 0
@@ -25,22 +34,24 @@ class MainViewModel @Inject constructor(private val repository: SearchRepository
     private var maxOffset: Int? = null
     var isLastPage = false
 
-
     fun searchItemsByQuery(query: String) {
         _uiState.value = MainActivityUiState.LoadingNewItems
         currentQuery = query
+        currentOffset = 0
         viewModelScope.launch {
             val result = repository.searchItemsByQuery(query, limit, currentOffset)
             when (result.status) {
                 Resource.Status.SUCCESS -> {
-                    result.data?.items?.let { _uiState.postValue(MainActivityUiState.SuccessNewItems(it)) }
-                        ?: run { _uiState.postValue(MainActivityUiState.Error(result.message.orEmpty())) }
-                    maxOffset = (result.data?.total)?.div(10)
-                    currentOffset = 0
+                    result.data?.items?.let {
+                        _currentItems.value = it
+                        _uiState.value = MainActivityUiState.SuccessNewItems(it)
+                        maxOffset = (result.data?.total)?.div(10)
+                    } ?: run {
+                        _uiState.value = MainActivityUiState.Error(result.message.orEmpty())
+                    }
                 }
-
                 Resource.Status.ERROR -> {
-                    _uiState.postValue(MainActivityUiState.Error(result.message.orEmpty()))
+                    _uiState.value = MainActivityUiState.Error(result.message.orEmpty())
                 }
             }
             isLastPage = false
@@ -48,20 +59,27 @@ class MainViewModel @Inject constructor(private val repository: SearchRepository
     }
 
     fun loadMoreItems() {
+        if (_isLoadingMoreItems.value || isLastPage) return
         _uiState.value = MainActivityUiState.LoadingMoreItems
+        _isLoadingMoreItems.value = true
+
         currentOffset += 1
         viewModelScope.launch {
             val result = repository.searchItemsByQuery(currentQuery, limit, currentOffset)
             when (result.status) {
                 Resource.Status.SUCCESS -> {
-                    result.data?.items?.let { _uiState.postValue(MainActivityUiState.SuccessMoreItems(it)) }
-                        ?: run { _uiState.postValue(MainActivityUiState.Error(result.message.orEmpty())) }
+                    result.data?.items?.let {
+                        _currentItems.value += it
+                        _uiState.value = MainActivityUiState.SuccessMoreItems(it)
+                    } ?: run {
+                        _uiState.value = MainActivityUiState.Error(result.message.orEmpty())
+                    }
                 }
-
                 Resource.Status.ERROR -> {
-                    _uiState.postValue(MainActivityUiState.Error(result.message.orEmpty()))
+                    _uiState.value = MainActivityUiState.Error(result.message.orEmpty())
                 }
             }
+            _isLoadingMoreItems.value = false
             isLastPage = currentOffset >= (maxOffset ?: 0)
         }
     }
@@ -73,13 +91,14 @@ class MainViewModel @Inject constructor(private val repository: SearchRepository
             when (result.status) {
                 Resource.Status.SUCCESS -> {
                     result.data?.let {
+                        _uiState.value = MainActivityUiState.Idle
                         _navigationEvent.postValue(MainActivityEvent(item.copy(pictures = it.pictures)))
                     } ?: run {
-                        _uiState.postValue(MainActivityUiState.Error(result.message.orEmpty()))
+                        _uiState.value = MainActivityUiState.Error(result.message.orEmpty())
                     }
                 }
                 Resource.Status.ERROR -> {
-                    _uiState.postValue(MainActivityUiState.Error(result.message.orEmpty()))
+                    _uiState.value = MainActivityUiState.Error(result.message.orEmpty())
                 }
             }
         }
